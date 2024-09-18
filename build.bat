68edbcb3900d629ca35cbaa3c18598b5dc0ec5b0
Kirurobo <kirurobo@gmail.com>
2024-09-18 18:13:21 +0900

    README.md はビルド時にルートのファイルをコピーするものとした。

--- build.bat
+++ build.bat
@@ -6,10 +6,13 @@ cd /d %~dp0
 rem Move to the target directory
 cd src
 
-rem Copy the readme file
+rem Copy the readme file temporarily
 copy ..\README.md .\README.txt
 
 rem Create a xpi file
 7z a ..\custom-subject-column@kirurobo.com.xpi *
+
+rem Remove the readme file
+del .\README.txt
 
 rem pause
