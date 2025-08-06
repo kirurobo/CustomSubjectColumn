d2da33ef69bed68f38eb2261105ab2491f53bf43
Kirurobo <kirurobo@gmail.com>
2025-08-06 12:34:32 +0900

    ビルド時、7zが無い場合はエラーメッセージを表示するようにした

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 @echo off
 
+rem Remember the current directory
+set CURDIR=%cd%
+
 rem Move to the directory where this script is located
 cd /d %~dp0
 
@@ -11,8 +14,22 @@ copy ..\README.md .\README.txt
 
 rem Create a xpi file
 7z a ..\custom-subject-column@kirurobo.com.xpi *
+set ERRORCODE=%errorlevel%
+rem remember the error code
 
 rem Remove the readme file
 del .\README.txt
 
+rem Move back to the original directory
+cd /d %CURDIR%
+
+
+rem If the 7z command failed, print an error message and exit
+if %ERRORCODE% neq 0 (
+    echo Error: 7z command failed! ^(Error code: %ERRORCODE%^)
+    pause
+    exit /b %ERRORCODE%
+)
+
+rem Pause (Commented out)
 rem pause
